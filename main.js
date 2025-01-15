a023ab56692d54b23f4477c28b5c495350f9711a
yanakorsak <kazantip2310@gmail.com>
2025-01-15 01:57:52 +0100

    случайное значение

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 //позиция коробля
-const location1= 3
-const location2= 4
-const location3 = 5
+const randomLoc = 2 // от 0 и до 4
+const location1 = randomLoc
+const location2 = location1 +1
+const location3 = location2 + 1
 
 // потоплен или нет
 let isSunk = false
 // предролагаемая позиция коробля (выстрел)
-let guess;
+let guess
 //добавляем счетчик попаданий
-let hits =0
+let hits = 0
 
-while (isSunk===false) {
+while (isSunk === false) {
   // не забываем, что строку нужно превратить в чило
-  guess = +prompt ("Fire! enter a number 0-6")
-  // логическое "или"
-  if(guess ===location1 || guess===location2 || guess===location3)
-    {
-    alert("HIT!")
-    // считаем попадания
-    hits=hits + 1
-    // если подбили
-    if(hits===3){
-    isSunk=true
-    alert("You sank the battleship");
-  } 
-}else{
-    alert("MISS");
+  guess = +prompt('Fire! enter a number 0-6')
+  if (guess < 0 || guess > 6) {
+    alert('Please, enter a valid cell number')
+  } else {
+    // логическое "или"
+    if (guess === location1 || guess === location2 || guess === location3) {
+      alert('HIT!')
+      // считаем попадания
+      hits = hits + 1
+      // если подбили
+      if (hits === 3) {
+        isSunk = true
+        alert('You sank the battleship')
+      }
+    } else {
+      alert('MISS')
+    }
   }
-
 }
