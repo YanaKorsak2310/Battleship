8be64be6a256d756b88561bd1990882433025bd2
yanakorsak <kazantip2310@gmail.com>
2025-01-15 02:11:00 +0100

    update stats

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 //позиция коробля
-const randomLoc = 2 // от 0 и до 4
+const randomLoc = Math.floor(Math.random()*5) // от 0 и до 4
 const location1 = randomLoc
 const location2 = location1 +1
 const location3 = location2 + 1
 
 // потоплен или нет
-let isSunk = false
+let isSunk = false;
 // предролагаемая позиция коробля (выстрел)
-let guess
+let guess;
 //добавляем счетчик попаданий
-let hits = 0
+let hits = 0;
+//создаем счетчик выстрелов
+let guesses =0;
+
 
 while (isSunk === false) {
   // не забываем, что строку нужно превратить в чило
@@ -17,6 +20,8 @@ while (isSunk === false) {
   if (guess < 0 || guess > 6) {
     alert('Please, enter a valid cell number')
   } else {
+    //увеличим счетчик выстрелов
+    guesses=guesses + 1
     // логическое "или"
     if (guess === location1 || guess === location2 || guess === location3) {
       alert('HIT!')
@@ -32,3 +37,5 @@ while (isSunk === false) {
     }
   }
 }
+const stats = 'You have'+ hits + " hits out off" + guesses + " shots."
+alert(stats)
